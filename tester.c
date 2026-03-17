a244967b401747685b4287b3c1102268f00234ac
Maristanez <Michaelbryan857@gmail.com>
2026-03-17 19:44:32 -0400

    Initial commit for EECS 2031 Practice

---
+++ tester.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void reverse_and_double(int arr[], int n) {
+  int i;
+  int j = n - 1;
+
+  for (i = 0; i < n / 2; i++) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    j--;
+  }
+
+  for (i = 0; i < n; i++) {
+    if (arr[i] % 2 == 0) {
+      arr[i] = arr[i] * 2;
+    }
+  }
+}
+
+int main() {
+  int num[6] = {1, 2, 3, 4, 5, 6};
+
+  reverse_and_double(num, 6);
+
+  // Print the result to test it
+  for (int i = 0; i < 6; i++) {
+    printf("%d ", num[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
