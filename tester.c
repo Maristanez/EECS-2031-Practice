a6314836a88d8f6c8a9ffc26439860e5148df1a8
Maristanez <Michaelbryan857@gmail.com>
2026-03-17 20:15:13 -0400

    Second Question

--- tester.c
+++ tester.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+void reverse_and_double(int arr[], int n);
+
+int main() {
+  int num[6] = {1, 2, 3, 4, 5, 6};
+
+  reverse_and_double(num, 6);
+
+  // Print the result to test it
+  for (int i = 0; i < 6; i++) {
+    printf("%d ", num[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
+
 void reverse_and_double(int arr[], int n) {
   int i;
   int j = n - 1;
@@ -16,18 +32,4 @@ void reverse_and_double(int arr[], int n) {
       arr[i] = arr[i] * 2;
     }
   }
-}
-
-int main() {
-  int num[6] = {1, 2, 3, 4, 5, 6};
-
-  reverse_and_double(num, 6);
-
-  // Print the result to test it
-  for (int i = 0; i < 6; i++) {
-    printf("%d ", num[i]);
-  }
-  printf("\n");
-
-  return 0;
 }
